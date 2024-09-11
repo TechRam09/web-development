c16890256e7bb2f30efde558fb6b68c6d4ba2f84
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 12:54:44 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,14 @@
+fruits =  ["pear",'melon','orange']
+vegetables = ["beans",'carrot','onion','tamoato']
+result = fruits + vegetables
+print(fruits)
+print(result)
+
+
+# fruits.insert(0,'apple')
+# print(fruits)
+
+
+# fruits.insert(-2,'mango')
+# print(fruits[3:6])
+# print(type(fruits))
