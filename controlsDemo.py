e59fcf71442ba66b6cbfdf9c51ad7a2f63e2377b
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 11:15:28 +0530

    Create controlsDemo.py

---
+++ controlsDemo.py
@@ -0,0 +1,12 @@
+# name = "Shreeram"
+
+# # loop on string
+# for i in name:
+#     print(i, end=", ")
+
+
+#loop on tuple
+
+color = ('red','blue','green','purple')
+for i in color:
+    print(i,end=' ')
