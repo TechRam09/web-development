c910482a0ad51c962788ff8e4d4bbc257b4a8af1
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 15:15:51 +0530

    Create tupleDemo.py

---
+++ Tuples/tupleDemo.py
@@ -0,0 +1,22 @@
+numbers = (2,3,4,6,7,24,66,69,96,88)
+print(numbers)
+
+# print(numbers[7])
+
+print(numbers[7])
+
+names = ("shree",'ram','keerthi','nishanth')
+print(names)
+
+mixValues = ("marks","name",1,2,3,4,5)
+
+print(mixValues)
+print(type(mixValues))
+
+values = 1,2,3,4,5,6,7
+print(type(values))
+
+
+stock = (2,3,4,5,6,7,24,48,90)
+
+print(stock[6:])
