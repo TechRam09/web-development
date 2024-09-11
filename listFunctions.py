3d6f95e67a16a7ac574ccfc3a332dc0e39be9011
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 12:54:47 +0530

    Create listFunctions.py

---
+++ listFunctions.py
@@ -0,0 +1,52 @@
+customer1 = ["Shree",1]
+customer2 = ["Shreeram",2]
+customer3 = ["keerti",3]
+customer4 = ["Nishath",4]
+
+
+customers = [customer1,customer2,customer3,customer4]
+
+# # print(customers)
+
+# for customer in customers:
+#     print(customer)
+    
+# #len()
+
+# print(len(customers))
+
+numbers = [23,45,66,87,92]
+# print(max(numbers))
+
+alphabets = ['K','Y','G','S']
+# print(max(alphabets))
+
+# print(sorted(numbers))
+# print(sorted(alphabets))
+
+#del keyword
+
+# stock = [23,24,27,28,32,22]
+
+# # del stock[1:4]
+# # del stock[:] => []
+
+# # del stock
+
+# print(stock)
+
+# price_a = [10,20,30]
+# price_b = [40,50,60]
+
+# price_a.extend(price_b)
+
+# print(price_a)
+
+
+student = ['shree','ram','ram','Nish']
+# print(student.count('ram'))
+
+
+# reverse the list
+
+print( student.reverse())
