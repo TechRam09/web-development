ba5d48b1ef8d0f94e30ea78e1fdec58f13ec937e
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 10:54:03 +0530

    Create LocalnGlobalScope.py

---
+++ LocalnGlobalScope.py
@@ -0,0 +1,11 @@
+icecream = "Chocolate"
+
+def food():
+    fruit = "apple"
+    vegetable = "beans"
+    print(fruit + " is Keerthi's faviourite fruit")
+    print(vegetable + " is Keerthi's faviourite vegetable")
+    
+food()
+print(icecream)
+print(fruit)
