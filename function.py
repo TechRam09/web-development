38c37651369b96e1348128b0d131b35a7d3e658c
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 10:53:56 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,14 @@
+# def fun():
+#     print("Hello world")
+
+# fun()
+
+
+def evenOrOdd(x):
+    if( x % 2 == 0):
+        print(x," is Even")
+    else:
+        print(x, " is Odd")
+        
+evenOrOdd(4)
+evenOrOdd(5)
