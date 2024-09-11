4a8c12bc69472e5aed17b7553fab67db3ccf2f46
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 15:15:53 +0530

    Create operations.py

---
+++ Tuples/operations.py
@@ -0,0 +1,13 @@
+# #repeat 
+stocks = (2,3,4,5,6,7,24,48,90)
+
+# print(stocks * 3)
+
+
+# #checking whether item exist i  tuple -  membership operator
+# print(7 in stocks)
+# print(69 in stocks)
+
+
+for stock in stocks:
+    print(stock, end=" ")
