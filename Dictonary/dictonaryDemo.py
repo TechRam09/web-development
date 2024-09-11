4b9ea7acd7c9c8c58789243c8c8f20fa5ec06326
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 15:46:58 +0530

    Update dictonaryDemo.py

--- Dictonary/dictonaryDemo.py
+++ Dictonary/dictonaryDemo.py
@@ -1,3 +1,40 @@
 student = {"name":"shree","age":20,"branch": "cse"}
+# print(student)
+# print(type(student))
+
+
+# add  value
+student["college"] = "VVIET"
+# print(student)
+
+# add a list   
+student["marks"] = [48,55,56,60,53]
+# print(student)
+
+#access
+# print(student['marks'])
+
+# print(student.get("age"))
+
+
+#update existing value
+student["age"] = 23
+# print(student)
+
+#remove the element 
+del student["college"]
+# print(student)
+
+
+#items() => print all items in value of tuples
+print(student.items())
+
+#keys() => returns only keys
+print(student.keys())
+
+#values() => returns only.values
+print(student.values())
+
+#clear() => returns a empty dictionary
+student.clear()
 print(student)
-print(type(student))
