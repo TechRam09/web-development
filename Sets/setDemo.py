98080e21b5f119d540c82eadd2c6831f2f0f1bc0
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 15:16:00 +0530

    Create setDemo.py

---
+++ Sets/setDemo.py
@@ -0,0 +1,24 @@
+
+number = {21,43,455,56,67,'manoj'}
+# print(number)
+print(type(number))
+
+# add element
+number.add(69)
+print(number)
+
+# delet the element
+number.remove(43)
+print(number)
+
+
+# pop the Element
+number.pop()
+print(number)
+
+del number
+# print(number)
+
+
+
+
