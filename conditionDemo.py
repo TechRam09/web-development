cd35b7a4c5841fe9b5714ba5764796427e5ad8fa
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 11:05:08 +0530

    Create conditionDemo.py

---
+++ conditionDemo.py
@@ -0,0 +1,24 @@
+x=10
+y=20
+
+if(x == 10):
+    print("y is", y)
+    
+
+student = "Shree"
+if(student == "Shree"):
+    print("Entry to college")
+else:
+    print("No Entry")
+    
+#"elif"
+
+a =30
+b =20
+
+if(a > b):
+    print("a is greater than b")
+elif(a == b):
+    print("a is equal to b")
+else:
+    print("a is less than b")
