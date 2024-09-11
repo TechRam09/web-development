ec46493719a1d32e84b021ea4da9ec871e297cce
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 12:55:28 +0530

    Create typeOfList.py

---
+++ typeOfList.py
@@ -0,0 +1,13 @@
+customer1 = ["Shree",1]
+customer2 = ["Shreeram",2]
+customer3 = ["keerti",3]
+customer4 = ["Nishath",4]
+
+
+customers = [customer1,customer2,customer3,customer4]
+
+# print(customers)
+
+for customer in customers:
+    print(customer)
+    
