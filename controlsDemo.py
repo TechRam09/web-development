a244d7129eed9291b6b03f43991faf872a845ef9
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-11 12:54:42 +0530

    Update controlsDemo.py

--- controlsDemo.py
+++ controlsDemo.py
@@ -7,6 +7,22 @@
 
 #loop on tuple
 
-color = ('red','blue','green','purple')
-for i in color:
-    print(i,end=' ')
+# color = ('red','blue','green','purple')
+# for i in color:
+#     print(i,end=' ')
+    
+    
+#in range
+
+# for k in range(5):
+#     print(k, end=" ")
+
+# for k in range(4,7):
+#     print(k,end=" ")
+
+
+
+count = 5
+while(count > 0):
+    print(count)
+    count= count -1
