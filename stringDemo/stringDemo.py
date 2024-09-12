807c79459d64ef640247e6c6331d2abc57575cc8
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-12 12:45:34 +0530

    Create stringDemo.py

---
+++ stringDemo/stringDemo.py
@@ -0,0 +1,47 @@
+name ="Shreeram N S"
+capsName = "SHREERAM.N.S"
+# print(name)
+
+#acces the particular character in string
+# print(name[0]) #s
+
+#slicing
+# print(name[0:5])
+
+# print(name[0:6:2])
+
+#isLower()
+# print(name.islower())
+
+# #isupper()
+# print(capsName.isupper())
+
+# #istile() => checks the string is in title case
+# print(name.istitle())
+
+# #isalpha()
+# print(name.isalpha())
+
+# #isalnum()
+# print(name.isalnum())
+
+# #isdigit()
+# print(name.isdigit())
+
+#split()
+# print(capsName.split('.'))
+
+# #upper =>converts to upper case
+# print(name.upper())
+
+#lower = > converts to lower case
+# print(name.lower())
+
+
+#capitalise => capitalise
+# print(name.capitalize())
+
+#replace
+print(name.replace("ram","lakshmi"))
+
+
