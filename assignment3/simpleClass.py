356cbda779b5425eafdaa8b49e97dbe5cb0bbc23
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-16 23:58:56 +0530

    Create simpleClass.py

---
+++ assignment3/simpleClass.py
@@ -0,0 +1,18 @@
+#simple employee class:
+
+class emp_details:
+    def emp1(self):
+        print("Employee 1: Nishnath")
+
+    def emp2(self):
+        print("employee 2 : Sumit")
+
+    def emp3(self):
+        print("employee 3: Shreeram")
+
+emp = emp_details()
+emp.emp1()
+emp.emp2()
+emp.emp3()
+
+print("\n========\n")
