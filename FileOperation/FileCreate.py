da88dade7a239a7d4c3d2b666cd47485904721f4
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-17 23:12:57 +0530

    Create FileCreate.py

---
+++ FileOperation/FileCreate.py
@@ -0,0 +1,26 @@
+# try:
+#     file = open("Nimbe.txt",'x')
+# except FileExistsError:
+#     print("File Already Exis")
+    
+# try:
+#     file = open("Nishanth.txt")
+# except FileExistsError:
+#     print("File Already Exist")
+    
+# try:
+#     file = open("Nishu.txt")
+# except FileExistsError:
+#     print("File Already Exist")
+    
+    
+# try:
+#     file = open("Shree.txt")
+# except FileExistsError:
+#     print("File Already Exist")
+    
+# try:
+#     file = open("Ram.txt")
+# except FileExistsError:
+#     print("File Already Exist")
+
