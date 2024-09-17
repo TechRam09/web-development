2dea4e63f0a6ce3f836b1db4e9dc94675abc20fe
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-17 23:13:03 +0530

    Create fileRead.py

---
+++ FileOperation/fileRead.py
@@ -0,0 +1,21 @@
+# nimbe = open('Nimbe.txt','r')
+# content = nimbe.read()
+# print(content)
+# nimbe.close()
+
+
+# # single line print 
+# f = open('Nishanth.txt','r')
+# line  = f.readline()
+# print(line)
+# f.close()
+
+
+#  multiple line read
+# f = open('Nishanth.txt','r')
+# lines  = f.readline()
+# for line in lines:
+#     print(line)
+#     lines= f.readline()
+# f.close()
+
