c9105820db4b199daaa3b7b826f86ff1649909e1
TechRam09 <144241655+TechRam09@users.noreply.github.com>
2024-09-17 23:13:21 +0530

    Create simpleClass.py

---
+++ FileOperation/simpleClass.py
@@ -0,0 +1,16 @@
+#simple employee class:
+
+class emp_details:
+    def emp1(self):
+        print("Employee 1: Nishnath")
+
+    def emp2(self):
+        print("employee 2 : Sumit")
+
+    def emp3(self):
+        print("employee 3: Shreeram")
+
+emp = emp_details()
+emp.emp1()
+emp.emp2()
+emp.emp3()
